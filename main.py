6d24a38397ff756bdfc0421d03630e41bfe692e0
Pratik Gurung <pratikgurung022@gmail.com>
2026-05-23 15:34:39 -0700

    Learned back about basic dicts

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("hello we will add more stuffs later in next 15-20 mins")
+
+userRepo ={
+    "name": "Pratik23G",
+    "project": "Junior Python role",
+    "target_days": 14,
+}
+
+userRepo["email"] = "pratikgurung022@gmail.com"
+
+print(userRepo.get("phone", "n/a"))
+
+print(userRepo)
+
+view = list(userRepo.keys()), list(userRepo.values())
+
+print(view)
